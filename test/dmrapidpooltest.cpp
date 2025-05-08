f5470a87fcc03af7b214d212e877d7fb7ebc5af2
brinkqiang <brink.qiang@gmail.com>
2025-05-08 09:45:58 +0800

    add

--- test/dmrapidpooltest.cpp
+++ test/dmrapidpooltest.cpp
@@ -74,7 +74,7 @@ int main(int argc, char* argv[]) {
         });
     }
 
-    std::this_thread::sleep_for(std::chrono::seconds(2));
+    pool.wait_idle();
     std::cout << DMGetPoolInfo();
 
     return 0;
