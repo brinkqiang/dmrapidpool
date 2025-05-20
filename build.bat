50b604677d3c77e46d3fe48ca2c643590a75b97f
brinkqiang <brink.qiang@gmail.com>
2025-05-20 22:36:24 +0800

    add

--- build.bat
+++ build.bat
@@ -1,10 +1,11 @@
 
 
-rmdir /S /Q build
+rmdir /S /Q build 2> nul
 mkdir build
-pushd build
+cd build
 cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo ..
 cmake --build . --config relwithdebinfo -- /m:%NUMBER_OF_PROCESSORS%
-popd
+echo "cd build && ctest -C relwithdebinfo --output-on-failure --output-junit report.xml"
+cd ..
 
 rem pause
